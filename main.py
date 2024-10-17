8af1190ba3bdfe811cdd58c1ff63ce1d02591452
Ahsanadam <adamebadul@gmail.com>
2024-10-18 00:11:51 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,38 @@
+from cryptography.fernet import Fernet
+
+
+def generate_key():
+    key = Fernet.generate_key()
+    with open("secret.key", "wb") as key_file:
+        key_file.write(key)
+
+
+def load_key():
+    return open("secret.key", "rb").read()
+
+
+def encrypt_password(password, key):
+    f = Fernet(key)
+    encrypted_password = f.encrypt(password.encode())
+    return encrypted_password
+
+
+def decrypt_password(encrypted_password, key):
+    f = Fernet(key)
+    decrypted_password = f.decrypt(encrypted_password).decode()
+    return decrypted_password
+
+
+try:
+    key = load_key()
+except FileNotFoundError:
+    generate_key()
+    key = load_key()
+
+
+password = "mySecurePassword"
+encrypted = encrypt_password(password, key)
+print(f"Encrypted password: {encrypted}")
+
+decrypted = decrypt_password(encrypted, key)
+print(f"Decrypted password: {decrypted}")
